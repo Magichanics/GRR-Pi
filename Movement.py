bd53036ff19d638243d14134dbaf92ab4a52cccf
Jan <42362229+Magichanics@users.noreply.github.com>
2019-04-16 17:57:44 -0400

    added pause between turns

--- Movement.py
+++ Movement.py
@@ -19,16 +19,20 @@ try:
     while True:
         DR_status = GPIO.input(DR)
         DL_status = GPIO.input(DL)
-#       print(DR_status,DL_status)
         if((DL_status == 0) or (DR_status == 0)):
+
+            # move 90 degrees left
             Ab.left()
-            #Ab.right()
-            time.sleep(0.5)
+            time.sleep(0.15)
+
+            # stop so the sensors will not trigger again
             Ab.stop()
-        #   print("object")
+            time.sleep(0.5)
+
         else:
+
+            # move forward
             Ab.forward()
-        #   print("forward")
 
 except KeyboardInterrupt:
     GPIO.cleanup();
