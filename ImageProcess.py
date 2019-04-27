a0791ce090387fcf0e3e1e9829e427d477ed6f26
Matu67 <49648109+Matu67@users.noreply.github.com>
2019-04-27 16:07:17 -0400

    All UI and image processing components

--- ImageProcess.py
+++ ImageProcess.py
@@ -7,34 +7,27 @@ from Mapping import Mapping
 from PIL import Image
 from PIL import ImageDraw
 
-map_obj = Mapping()
-map_obj.create_map(3)
-map_obj.set_element(-2, -2, 'r')
-map_obj.set_element(2, 2, 'r')
-map_obj.set_element(-2, 2, 'r')
-map_obj.set_element(9, -2, 'g')
 
-for i in range(len(map_obj.grid)):
-    print(map_obj.grid[i])
+class ImageProcess:
+    # makes grid image
+    def make_grid_image(self, map_obj):
+        x = len(map_obj.grid[0])
+        y = len(map_obj.grid)
 
-x = len(map_obj.grid[0])
-y = len(map_obj.grid)
+        img = Image.new('RGB', (x * 50, y * 50), color=(255, 255, 255))
+        draw = ImageDraw.Draw(img)
 
-img = Image.new('RGB', (x*50, y*50), color=(255, 255, 255))
-draw = ImageDraw.Draw(img)
+        fill_colour = ""
+        for row in range(x):
+            for col in range(y):
+                if map_obj.grid[col][row] == "R":
+                    fill_colour = "black"
+                elif map_obj.grid[col][row] == "r":
+                    fill_colour = "red"
+                elif map_obj.grid[col][row] == "g":
+                    fill_colour = "green"
+                else:
+                    fill_colour = "white"
+                draw.rectangle([(row * 50, col * 50), (row * 50 + 49, col * 50 + 49)], fill=fill_colour, outline="black")
 
-fillColour = ""
-for row in range(x):
-    for col in range(y):
-        print(col, row)
-        if map_obj.grid[col][row] == "R":
-            fillColour = "black"
-        elif map_obj.grid[col][row] == "r":
-            fillColour = "red"
-        elif map_obj.grid[col][row] == "g":
-            fillColour = "green"
-        else:
-            fillColour = "white"
-        draw.rectangle([(row * 50, col * 50), (row * 50 + 49, col * 50 + 49)], fill=fillColour, outline="black")
-
-img.save("rectangle.png")
+        img.save("rectangle.png")
