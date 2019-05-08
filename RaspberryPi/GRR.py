1702fdf0d2b22c651d138b4d3111742dcf081b80
Jan <42362229+Magichanics@users.noreply.github.com>
2019-05-07 21:58:20 -0400

    added left and right turn based on sensors

--- RaspberryPi/GRR.py
+++ RaspberryPi/GRR.py
@@ -12,20 +12,26 @@ class GRR(BotFunctions, BotLogger):
 
     # get the current angle and total distance, and add to list
     def debug_log(self):
-        self.dl.append(str(len(self.dl) - 1) + ',' + str(self.total_distance) + ',' \
-                       + str(self.curr_angle) + ',' + str(time.time() - self.init_time))
-
+        if self.debug_mode:
+            self.dl.append(str(len(self.dl) - 1) + ',' + str(self.total_distance) + ',' \
+                           + str(self.curr_angle) + ',' + str(time.time() - self.init_time))
+        else:
+            pass
+        
     # save log to text
     def export_debug_log(self, save_loc='debug_logs.txt'):
-        with open(save_loc, 'w') as f:
+        if self.debug_mode:
+            with open(save_loc, 'w') as f:
 
-            # write csv headings
-            f.write(',total_distance,angle,seconds')
+                # write csv headings
+                f.write(',total_distance,angle,seconds')
 
-            # write csv contents
-            for item in self.dl:
-                f.write(item + '\n')
-            f.close()
+                # write csv contents
+                for item in self.dl:
+                    f.write(item + '\n')
+                f.close()
+        else:
+            pass
 
     def bot_run(self):
 
@@ -34,29 +40,26 @@ class GRR(BotFunctions, BotLogger):
             while True:
 
                 # check left sensors
-                if GPIO.input(self.DL) == 0:
-                    
-                    self.print_map()
+                if GPIO.input(self.DL) == 0: # simplify
                     
                     # turn right until it detects a block
                     while GPIO.input(self.DL) == 0:
                         self.block_detected()
-                        self.right()
+                        self.left()
                     self.stop()
                 
                 # check right sensors
-                elif GPIO.input(self.DR) == 0:
-                    
-                    self.print_map()
+                if GPIO.input(self.DR) == 0:
                     
                     # turn left until it detects a block
                     while GPIO.input(self.DR) == 0:
                         self.block_detected()
-                        self.left()
+                        self.right()
                     self.stop()
 
                 # move forward; calculate distance
-                else:
+                #else:
+                while (GPIO.input(self.DR) and GPIO.input(self.DL)):
 
                     # get distance/angle using ultrasonic?
                     self.curr_angle = self.get_angle()
@@ -69,8 +72,10 @@ class GRR(BotFunctions, BotLogger):
                     # log forward movement
                     self.forward_log(self.total_distance, self.curr_angle)
                     
-                    # stop robot to prevent inaccuracies
-                    self.stop()
+                # stop robot to prevent inaccuracies
+                self.stop()
+                
+                self.print_map()
 
         # stops when Ctrl+C
         except KeyboardInterrupt:
@@ -79,8 +84,9 @@ class GRR(BotFunctions, BotLogger):
             GPIO.cleanup()
 
     # create bot
-    def __init__(self):
-        BotFunctions.__init__(self)
+    def __init__(self, debug_mode=False):
+        self.debug_mode = debug_mode
+        BotFunctions.__init__(self, cycle=20, frequency=200)
         BotLogger.__init__(self)
         self.total_distance = 0
         self.dl = []
